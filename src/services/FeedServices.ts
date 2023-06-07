adfe5b3ab9f900d4570f86226257f317f6a13421
Eliane Henriqueta <elianehenriqueta@gmail.com>
2023-06-07 09:19:50 -0300

    feat:Criar tela de feed

---
+++ src/services/FeedServices.ts
@@ -0,0 +1,12 @@
+import { useAccessTokenStore } from '../stores/accessToken';
+import { HttpApiServices } from './HttpApiServices';
+
+export class FeedServices extends HttpApiServices {
+    async getFeedPrincipal() {
+        return await this.get('/feed');
+    }
+
+    async getFeedPorId(id: String) {
+        return await this.get('/feed?id=' + id);
+    }
+}
