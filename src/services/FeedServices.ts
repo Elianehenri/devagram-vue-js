8db0196f76aa4f09a82f57c6ca6137780636ee42
Eliane Henriqueta <elianehenriqueta@gmail.com>
2023-06-07 10:47:11 -0300

    feat: Criar açoes de curtir e comentar.

--- src/services/FeedServices.ts
+++ src/services/FeedServices.ts
@@ -9,4 +9,14 @@ export class FeedServices extends HttpApiServices {
     async getFeedPorId(id: String) {
         return await this.get('/feed?id=' + id);
     }
+
+    async togglCurtir(id : String){
+        return this.put(`/like?id=${id}`);
+    }
+
+    async enviarComentario(id: String, comentario: String) {
+        return this.put(`/comentario?id=${id}`, {
+            comentario
+        });
+    }
 }
